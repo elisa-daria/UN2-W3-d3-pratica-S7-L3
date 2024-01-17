9c1c85daee1656aa86cc2457202112ea25fea78d
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-01-17 18:06:02 +0100

    even better style

--- library.js
+++ library.js
@@ -21,11 +21,11 @@ const libraryAtYourDisposal = () => {
       const fillTheLibrary = () => {
         books.forEach((book) => {
           const newCol = document.createElement("div");
-          newCol.classList.add("col-12");
+          newCol.classList.add("col-10");
           newCol.classList.add("col-sm-6");
           newCol.classList.add("col-md-3");
           newCol.innerHTML = `
-              <div class="card h-100 w-100">
+              <div class="card h-100">
                   <img src="${book.img}" class="card-img-top" style="height:320px" alt="book-cover" />
                       <div class="card-body">
                            <h5 class="card-title fs-5" style="height:135px">${book.title}</h5>
